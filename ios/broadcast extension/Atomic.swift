24ae438e9f73b05150c11310fda0aaf3020d9075
dian00 <dian00hong@gmail.com>
2023-01-06 16:08:22 +0900

    [iOS] broadcast extension 추가

---
+++ ios/broadcast extension/Atomic.swift
@@ -0,0 +1,37 @@
+//
+//  Atomic.swift
+//  BroadcastExtension
+//
+//  Created by dian on 2023/01/03.
+//  Copyright © 2023 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+struct Atomic<Value> {
+
+    private var value: Value
+    private let lock = NSLock()
+
+    init(wrappedValue value: Value) {
+        self.value = value
+    }
+
+    var wrappedValue: Value {
+        get { load() }
+        set { store(newValue: newValue) }
+    }
+
+    func load() -> Value {
+        lock.lock()
+        defer { lock.unlock() }
+        return value
+    }
+
+    mutating func store(newValue: Value) {
+        lock.lock()
+        defer { lock.unlock() }
+        value = newValue
+    }
+}
